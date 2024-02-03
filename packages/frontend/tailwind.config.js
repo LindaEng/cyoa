d294931b751f62a1ba35e9fab3d06ee77f9eb16f
LindaEng <90lineng@gmail.com>
2024-02-02 22:47:11 -0500

    set up tailwind

---
+++ packages/frontend/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  darkMode: 'class', // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
