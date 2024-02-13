82a0ff344a01c6c5936c16b9a77f3870a4165460
LindaEng <90lineng@gmail.com>
2024-02-13 13:53:54 -0500

    Edit feature added for Learning Cards.

--- packages/frontend/src/components/LearningCard.jsx
+++ packages/frontend/src/components/LearningCard.jsx
@@ -1,15 +1,17 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect} from 'react';
+import { api } from "../api"
+
 
 export const LearningCard = ({ title, lesson }) => {
     const [isEditing, setIsEditing] = useState(false);
     const [editedTitle, setEditedTitle] = useState(title);
     const [editedLesson, setEditedLesson] = useState(lesson);
-   
+
     useEffect(() => {
         setEditedTitle(title);
         setEditedLesson(lesson);
     }, [title, lesson]);
-    
+
     const handleEditClick = () => {
         setIsEditing(true);
     };
@@ -19,32 +21,68 @@ export const LearningCard = ({ title, lesson }) => {
     };
 
     const handleTitleChange = (e) => {
-        setEditedTitle(e.target.innerText);
+        setEditedTitle(e.target.value);
     };
 
     const handleLessonChange = (e) => {
-        setEditedLesson(e.target.innerText);
+        setEditedLesson(e.target.value);
     };
+
+    const handleCancel = (e) => {
+        e.stopPropagation();
+        console.log('Reverting to:', title, lesson);
+        setEditedTitle(title);
+        setEditedLesson(lesson);
+        setIsEditing(false);
+    };
+    
+
+    const handleSave = async () => {
+        try {
+            const res = await api.put(`/api/lessons/${lesson._id}`, {
+                title: editedTitle,
+                lesson: editedLesson
+            });
+            console.log('res', res);
+        } catch (error) {
+            console.error(error);
+        }
+    }
 
     return (
         <div className={`border border-gray-900 shadow-lg flex flex-col justify-center items-center transition-all duration-300 m-2 p-4 rounded `}>
-            <button onClick={handleEditClick}>{(isEditing) ? 'Save' : 'Edit'}</button>
+            <div onClick={handleEditClick}>{(isEditing) ? <button onClick={handleSave}>Save</button> : 'Edit'}</div>
             {(isEditing) && (
-                <button onClick={handleBlur}>Cancel</button>
+                <button onMouseDown={handleCancel}>Cancel</button>
             )}
 
-            {editedTitle && (
+            {isEditing ? (
+                <input 
+                    onBlur={handleBlur}
+                    value={editedTitle} 
+                    onChange={handleTitleChange}
+                    className={`${isEditing ? 'ring-2 ring-blue-500' : ''} mb-2`}
+                />
+            ) : (
                 <h2 
                     onBlur={handleBlur} 
                     onInput={handleTitleChange} 
                     contentEditable={isEditing}
                     suppressContentEditableWarning={true}
-                    className={`${isEditing ? 'ring-2 ring-blue-500' : ''} mb-2`}
+                    className={`${isEditing ? 'ring-2 ring-blue-500' : ''}`}
                 >
                     {editedTitle}
                 </h2>
             )}
-            {editedLesson && (
+            {isEditing ? 
+                (
+                    <textarea 
+                        onBlur={handleBlur}
+                        value={editedLesson} 
+                        onChange={handleLessonChange}
+                        className={`${isEditing ? 'ring-2 ring-blue-500' : ''} mb-2 w-full h-[10rem]`}
+                    />)
+            : (
                 <p 
                     onBlur={handleBlur} 
                     onInput={handleLessonChange} 
